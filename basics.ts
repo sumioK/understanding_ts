3bc6d546294c5478dbf9bc2b29a179fadca084e2
sumiok <tap.miwo@gmail.com>
2023-03-31 05:51:18 +0900

    1

---
+++ basics.ts
@@ -0,0 +1,17 @@
+{
+  function add(n1: number, n2: number, showResult: boolean, phrase: string){
+    if(showResult){
+      const result = n1 + n2
+      console.log(phrase + result)
+    }else{
+      return result
+    }
+  }
+
+  const number1 = 5;
+  const number2 = 2.8;
+  const printResult = true;
+const resultPhrase = 'Result: '
+
+  const result = add(number1, number2, printResult, resultPhrase)
+}
