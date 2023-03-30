e87d8b3d7122b1776ff6ceb2c32d07caf3554095
sumiok <tap.miwo@gmail.com>
2023-03-31 05:56:26 +0900

    obj

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-{
-    function add(n1, n2, showResult, phrase) {
-        if (showResult) {
-            var result_1 = n1 + n2;
-            console.log(phrase + result_1);
-        }
-        else {
-            return result_2;
-        }
-    }
-    var number1 = 5;
-    var number2 = 2.8;
-    var printResult = true;
-    var resultPhrase = 'Result: ';
-    var result_2 = add(number1, number2, printResult, resultPhrase);
-}
+var person = {
+    name: 'yota',
+    age: 30,
+};
+console.log(person);
---
+++ app.ts
@@ -0,0 +1,11 @@
+{
+  const person:{
+    name: string;
+    age: number;
+  } = {
+    name: 'yota',
+    age:30,
+  }
+
+  console.log(person.name)
+}
