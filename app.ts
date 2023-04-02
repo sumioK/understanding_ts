24e9e3fe28e185448817b15c4ee3663032cecfed
sumiok <tap.miwo@gmail.com>
2023-04-02 16:48:35 +0900

    alius

--- app.ts
+++ app.ts
@@ -1,17 +1,33 @@
 {
-  function combine(input1: number | string, input2: number | string){
+  type Combinable = number | string
+  type conversionDescriptor = 'as-number'| 'as-text'
+
+  function combine(
+    input1: number | Combinable, 
+    input2: number | Combinable, 
+    resultConversion: conversionDescriptor,
+    ){
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number'){
-    result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || 
+      resultConversion === 'as-number'){
+    result = +input1 + +input2;
       return result
     }else{
       result = input1.toString() + input2.toString();
       return result;
     }
+    return result;
   }
 
-  const combinedAges = combine(30, 26);
+  const combinedAges = combine(30, 26, 'as-number');
   console.log(combinedAges)
-  const combinedNames = combine('Max', 'Anna')
+
+  const combinedStringAges = combine('30', '26', 'as-number');
+  console.log(combinedStringAges)
+
+  const combinedNames = combine('Max', 'Anna', 'as-text')
   console.log(combinedNames)
+
+
+
 }
