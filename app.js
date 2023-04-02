22e3328f4750fb074e303ecc38470318df3a514e
sumiok <tap.miwo@gmail.com>
2023-04-02 15:58:18 +0900

    tupple

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-var person = {
-    name: 'yota',
-    age: 30,
-};
-console.log(person);
+{
+    // const person:{
+    //   name: string;
+    //   age: number;
+    // } 
+    var person = {
+        name: 'yota',
+        age: 30,
+        hobbies: ['sports', 'Cooking'],
+    };
+    var favoriteActivities = void 0;
+    favoriteActivities = ['sprots'];
+    console.log(person.name);
+    for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+        var hobby = _a[_i];
+        console.log(hobby.toUpperCase());
+    }
+}
