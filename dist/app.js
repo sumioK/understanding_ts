a4780aeea4aa46090493ea3e9b851c2dbbb1e432
sumiok <tap.miwo@gmail.com>
2023-04-09 11:33:56 +0900

    arrow func

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,18 @@
 "use strict";
 {
+    const userName = 'Max';
+    let age = 30;
+    age = 29;
+    const add = (a, b) => a + b;
+    const printOutput = output => {
+        console.log(output);
+    };
+    console.log(add(3, 5));
     const button = document.querySelector('button');
-    button?.addEventListener('click', () => {
-        console.log('Clicked!');
-    });
+    if (button) {
+        button.addEventListener('click', event => {
+            console.log(event);
+        });
+    }
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
 {
-  const button = document.querySelector('button');
+  const userName = 'Max';
+// userName = 'Maximilian'
+  let age = 30
+  age = 29
+  // if(age >= 20){
+  //   let isAdult = true
+  // }
+  // console.log(isAdult)
 
-  button?.addEventListener('click',() => {
-    console.log('Clicked!')    
-  })
+  // function add(a: number, b: number){
+  //   let result;
+  //   result = a + b;
+  //   return result;
+  // }
+
+  const add = (a: number, b: number) =>  a + b
+
+  const printOutput: (output: string|number ) => void = output => {
+    console.log(output)
+  }
+  console.log(add(3, 5))
+  
+  const button = document.querySelector('button')
+  if(button){
+    button.addEventListener('click', event => {
+      console.log(event)
+    })
+  }
 }
